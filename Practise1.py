48dd084c6343dacfb850a29cba2a5ac840cf69a5
m0r4ey <plaksinaleks2306@mail.ru>
2024-09-04 13:14:55 +0500

    Вводное задание

---
+++ Practise1.py
@@ -0,0 +1,19 @@
+#Arifmetics
+print('1st program')
+print('(9^0,5)*5 = ', 9**0.5*5,'\n')
+
+#Logic
+print('2nd program')
+print(9.99>9.98 and 1000!=1000.1,'\n')
+
+#School riddle
+print('3rd program')
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2),'\n')
+
+#First after point
+print('4th program')
+x='123.456'
+x=float(x)
+print(int(x*10%10))
