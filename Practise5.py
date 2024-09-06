57e56ddcc52495e63eeef502bdd39358c5e005ac
m0r4ey <plaksinaleks2306@mail.ru>
2024-09-06 18:35:18 +0500

    Организация программ и методы строк

---
+++ Practise5.py
@@ -0,0 +1,9 @@
+#Организация программ и методы строк
+my_string = input("Введите строку: ")
+print ("Количество символов в строке:", len(my_string))
+
+print (my_string.upper())
+print (my_string.lower())
+print (my_string.replace(' ', ''))
+print (my_string[0])
+print (my_string[-1])
