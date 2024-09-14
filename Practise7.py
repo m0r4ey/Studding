3993d5d22394f62fb643a51e72c2ea35c31008e8
m0r4ey <plaksinaleks2306@mail.ru>
2024-09-14 16:45:03 +0500

    Словари и множества

--- Practise7.py
+++ Practise7.py
@@ -1,17 +1,17 @@
 #Словари и множества
 my_dict = {'Ilya' : 2000, 'Naslya' : 2001, 'Tatya' : 1998, 'Alex' : 2000}
 print (my_dict)
-print (my_dict['Ilya'])
-my_dict ['Sasha'] = 1995
-print (my_dict['Sasha'])
+print ('Ilya:', my_dict['Ilya'])
+print ('Vova:', my_dict.get('Vova'))
 my_dict.update ({'Valera' : 1999,
                 'Lukya' : 2002})
-my_dict.pop('Alex')
+deleted = my_dict.pop('Alex')
+print('Alex:', deleted)
 print(my_dict, '\n')
 
-my_set = {1, 3, "A", 4, 3, 4, 9, 9, "A", 1, 2, 2, 3, "A", "Chi", "Gaga", "B"}
+my_set = {1, 3, "A", 4, 3, 4, 9, 9, "A", 1, 2, 2, 3, "A", "B"}
 print (my_set)
 my_set.add(10)
-my_set.add("GH")
 my_set.add("light")
+my_set.discard(1)
 print (my_set)
